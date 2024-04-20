a6f3aeefadf28652a8705bd1e7cc998c76f1ca1b
tanmaykhatri07726@gmail.com <tanmaykhatri07726@gmail.com>
2024-04-20 23:08:34 +0530

    redux conf

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+import React, { useEffect, useState } from "react";
+import authService from "./appwrite/auth";
+import { Footer, Header } from "./components";
+import { useDispatch } from "react-redux";
+import { login, logout } from "./redux/authSlice";
+import { Outlet } from "react-router-dom";
 
 function App() {
-  return (
-    <div></div>
-  )
+  const [loading, setLoading] = useState(true);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    authService
+      .getCurrentUser()
+      .then((userData) => {
+        if (userData) {
+          dispatch(login(userData));
+        }
+        else{
+          dispatch(logout())
+        }
+      })
+      .finally(() => {
+        setLoading(false)
+      });
+  }, []);
+
+  return !loading ? (
+    <>
+    <div className=" h-screen flex flex-wrap content-between bg-gray-400">
+      <div className="w-full block">
+        <Header/>
+        <main>
+        {/* <Outlet/> todo outlet */}
+        </main>
+        <Footer/>
+      </div>
+    </div>
+    </>
+  ):null
 }
 
-export default App
+export default App;
