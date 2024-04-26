08364cdf11f4564c2b1e379d808f995a9506b362
tanmaykhatri07726@gmail.com <tanmaykhatri07726@gmail.com>
2024-04-26 14:59:16 +0530

    production grade component

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,12 @@
 import Header from "./Header/Header";
 import Footer from "./Footer/Footer";
-
+import Container from "./container/Container.jsx"
+import LogoutButton from "./Header/LogoutButton.jsx";
+import Logo from "./Logo.jsx";
 export {
     Header,
-    Footer
+    Footer,
+    Container,
+    LogoutButton,
+    Logo
 }
