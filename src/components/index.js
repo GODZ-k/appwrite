68b2d03e82aa4a12843a4612cb3ceacc0e7c7b9e
tanmaykhatri07726@gmail.com <tanmaykhatri07726@gmail.com>
2024-04-26 22:41:31 +0530

    authentication

--- src/components/index.js
+++ src/components/index.js
@@ -3,12 +3,16 @@ import Footer from "./Footer/Footer";
 import Container from "./container/Container.jsx"
 import LogoutButton from "./Header/LogoutButton.jsx";
 import Logo from "./Logo.jsx";
+import Input from "./Input.jsx";
 import Select from "./Select.jsx";
+import Button from "./Button.jsx";
 export {
     Header,
     Footer,
     Container,
     LogoutButton,
     Logo,
-    Select
+    Select,
+    Input,
+    Button
 }
